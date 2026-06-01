bcbbd29ddced1d2f908843ef07aa0df0c7b836cc
vikranth-07 <mandavasaivikranthgoud@gmail.com>
2026-06-01 21:45:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,24 +95,7 @@ if st.button("Update Status"):
     else:
         st.error("Task ID Not Found")
         
-        tasks = get_tasks(user)
-
 tasks = get_tasks(user)
-
-df = pd.DataFrame(
-    tasks,
-    columns=[
-        "Task ID",
-        "User",
-        "Task",
-        "Priority",
-        "Due Date",
-        "Status"
-    ]
-)
-
-st.dataframe(df, use_container_width=True)
-
 task_options = {
     f"{task[0]} - {task[2]}": task[0]
     for task in tasks
